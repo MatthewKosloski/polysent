b78bfbfb11244bac3aed271be90c43fb7c843cb2
MatthewKosloski <matthewkosloski@me.com>
2016-01-23 20:33:30 -0600

    renamed 'PollCtrl' to generic 'ctrl'

--- app/api/poll/index.js
+++ app/api/poll/index.js
@@ -3,19 +3,19 @@ var express = require('express'),
 
 // Controllers
 // -----------------------------------------------------
-var PollCtrl = require('./poll.controller');
+var ctrl = require('./poll.controller');
 
 // Routes
 // -----------------------------------------------------
-router.get('/search', PollCtrl.search); // ex., /api/search?q=music
-router.get('/poll/:id', PollCtrl.single); // ex., /api/poll/1234
-router.put('/poll/:id/upvote', PollCtrl.upvote); // ex., /api/poll/1234/upvote
-router.put('/poll/:id/vote', PollCtrl.vote); // ex., /api/poll/10/vote?decrement=-1&increment=0
-router.get('/random/:category', PollCtrl.random); // ex., /api/random/science
-router.get('/featured', PollCtrl.featured); // ex., /api/featured?page=1&per_page=24
-router.get('/newest', PollCtrl.newest); // ex., /api/newest?page=1&per_page=24
-router.get('/most-votes', PollCtrl.mostVotes); // ex., /api/most-votes?page=1&per_page=24
-router.get('/top-rated', PollCtrl.topRated); // ex., /api/top-rated?page=1&per_page=24
-router.post('/new', PollCtrl.create);
+router.get('/search', ctrl.search); // ex., /api/search?q=music
+router.get('/poll/:id', ctrl.single); // ex., /api/poll/1234
+router.put('/poll/:id/upvote', ctrl.upvote); // ex., /api/poll/1234/upvote
+router.put('/poll/:id/vote', ctrl.vote); // ex., /api/poll/10/vote?decrement=-1&increment=0
+router.get('/random/:category', ctrl.random); // ex., /api/random/science
+router.get('/featured', ctrl.featured); // ex., /api/featured?page=1&per_page=24
+router.get('/newest', ctrl.newest); // ex., /api/newest?page=1&per_page=24
+router.get('/most-votes', ctrl.mostVotes); // ex., /api/most-votes?page=1&per_page=24
+router.get('/top-rated', ctrl.topRated); // ex., /api/top-rated?page=1&per_page=24
+router.post('/new', ctrl.create); // /api/new
 
 module.exports = router;
