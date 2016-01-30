cd9503386409c921b3462cfcc4bf88348b8fffc5
MatthewKosloski <matthewkosloski@me.com>
2016-01-29 22:35:27 -0600

    trust proxy... again

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.disable('x-powered-by');
-// app.enable('trust proxy');
+app.enable('trust proxy');
 
 // CORS Support
 // ------------------------------------------------------
