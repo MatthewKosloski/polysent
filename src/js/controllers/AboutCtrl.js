83dae25416fc7f13740b3ca8038c4cc0dcdf1372
MatthewKosloski <matthewkosloski@me.com>
2016-01-29 22:05:51 -0600

    removed the about controller

--- src/js/controllers/AboutCtrl.js
+++
@@ -1,8 +0,0 @@
-(function(){
-	'use strict';
-	angular
-		.module('controllers')
-		.controller('AboutCtrl', ['$scope', function($scope){
-			$scope.stats = {polls: 0, visitors: 0, upvotes: 0};
-		}]);
-})();
